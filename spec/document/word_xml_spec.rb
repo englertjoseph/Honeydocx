a971d5f3b5d1781692bf662e67ae9313b82a4672
Joseph Englert <englerjosegithub@gmail.com>
2015-12-11 10:41:20 +1300

    Refactor specs

--- spec/document/word_xml_spec.rb
+++ spec/document/word_xml_spec.rb
@@ -11,17 +11,16 @@ describe Honeydocx::WordXML do
     context 'using template document' do
       before(:each) do
         @opts = { url: @url, token: @token }
+        @wordXML = dummy_document.create(:docx, @opts)
       end
 
       it 'should set path to template document if no path specified' do
-        wordXML = dummy_document.create(:docx, @opts)
-        expect(wordXML.path).to eq(Honeydocx::WordXML.blank_path)
+        expect(@wordXML.path).to eq(Honeydocx::WordXML.blank_path)
       end
 
       it 'should insert a token in the header rels' do
-        wordXML = dummy_document.create(:docx, @opts)
         expected_xml = File.open(File.expand_path('../../fixtures/header1.xml.rels', __FILE__)).read
-        expect(clean_xml(wordXML.header_rels_xml)).to eq (clean_xml(expected_xml))
+        expect(clean_xml(@wordXML.header_rels_xml)).to eq (clean_xml(expected_xml))
       end
     end
 
@@ -30,27 +29,24 @@ describe Honeydocx::WordXML do
         before(:each) do
           @path = File.expand_path('../../fixtures/header_no_rels.docx', __FILE__)
           @opts = { path: @path, url: @url, token: @token }
+          @wordXML = dummy_document.create(:docx, @opts)
         end
 
         it 'should set the path of the document' do
-          wordXML = dummy_document.create(:docx, @opts)
-          expect(wordXML.path).to eq(@opts[:path])
+          expect(@wordXML.path).to eq(@opts[:path])
         end
 
         it 'should create header rels with token' do
-          wordXML = dummy_document.create(:docx, @opts)
           expected_xml = File.open(File.expand_path('../../fixtures/header1.xml.rels', __FILE__)).read
-          expect(clean_xml(wordXML.header_rels_xml)).to eq (clean_xml(expected_xml))
+          expect(clean_xml(@wordXML.header_rels_xml)).to eq (clean_xml(expected_xml))
         end
 
         it 'should create a header rels file' do
-          wordXML = dummy_document.create(:docx, @opts)
-          expect(wordXML.send(:has_header_rels?)).to eq(true)
+          expect(@wordXML.send(:has_header_rels?)).to eq(true)
         end
 
         it 'should reference token in word/header1.xml', focus: true do
-          wordXML = dummy_document.create(:docx, @opts)
-          expect(clean_xml(wordXML.header_xml)).to eq (clean_xml(expected_header))
+          expect(clean_xml(@wordXML.header_xml)).to eq (clean_xml(expected_header))
         end
       end
 
@@ -58,25 +54,23 @@ describe Honeydocx::WordXML do
         before(:each) do
           @path = File.expand_path('../../fixtures/header_with_rels.docx', __FILE__)
           @opts = { path: @path, url: @url, token: @token }
+          @wordXML = dummy_document.create(:docx, @opts)
         end
 
         it 'should set the path of the document' do
-          wordXML = dummy_document.create(:docx, @opts)
-          expect(wordXML.path).to eq(@opts[:path])
+          expect(@wordXML.path).to eq(@opts[:path])
         end
 
         it 'should insert a token in the header rels' do
-          wordXML = dummy_document.create(:docx, @opts)
           expected_xml = File.open(File.expand_path('../../fixtures/header1_with_rels.xml.rels', __FILE__)).read
-          expect(clean_xml(wordXML.header_rels_xml)).to eq (clean_xml(expected_xml))
+          expect(clean_xml(@wordXML.header_rels_xml)).to eq (clean_xml(expected_xml))
         end
 
         it 'should retain the old rels' do
-          wordXML = dummy_document.create(:docx, @opts)
           old_rels = Nokogiri::XML(get_header_rels(@path))
           old_rels.remove_namespaces! # XML namespaces aren't correct and no searchable without removing
           old_rels.root.xpath(".//Relationship").each do |relation|
-            expect(wordXML.header_rels_xml).to include(relation)
+            expect(@wordXML.header_rels_xml).to include(relation)
           end
         end
       end
@@ -86,11 +80,11 @@ describe Honeydocx::WordXML do
       before(:each) do
         @path = File.expand_path('../../fixtures/blank_no_header.docx', __FILE__)
         @opts = { path: @path, url: @url, token: @token }
+        @wordXML = dummy_document.create(:docx, @opts)
       end
 
       it 'should set the path of the document' do
-        wordXML = dummy_document.create(:docx, @opts)
-        expect(wordXML.path).to eq(@opts[:path])
+        expect(@wordXML.path).to eq(@opts[:path])
       end
 
       it '' do
