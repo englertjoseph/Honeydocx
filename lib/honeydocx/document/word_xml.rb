0fa2283958086fd67f800fe7ae70c940075d959d
Joseph Englert <englerjosegithub@gmail.com>
2015-12-11 06:38:44 +1300

    Reader header rels correctly + fix partial ignoring 'o' namespace bug

--- lib/honeydocx/document/word_xml.rb
+++ lib/honeydocx/document/word_xml.rb
@@ -27,15 +27,30 @@ module Honeydocx
       elsif(has_header?)
         # Add resource && add to header1.xml
         if (!has_header_rels?)
-          create_header_rels
+          #create_header_rels
+          @header_rels_xml = template_header_rels
+          insert_partial
         else
           # Get last relationship number (rid)
           # Add relationship with last rid + 1
           # Edit partial to include rid
+          @header_rels_xml = zip.read("word/_rels/header1.xml.rels")
+          header_rels = Nokogiri::XML(@header_rels_xml)
+          relations_number = header_rels.children[0].children.collect {
+            |child| child["name"] == "Relationship" }.size
+          dict = { "Id" => "rId#{relations_number+1}",
+              "Type" => "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image",
+              "Target" => "TOKEN_URL",
+              "TargetMode" => "External"}
+          header_rels.children[0].add_child("<Relationship
+            Id = \"rId#{relations_number+1}\"
+            Type = \"http://schemas.openxmlformats.org/officeDocument/2006/relationships/image\"
+            Target = \"TOKEN_URL\"
+            TargetMode = \"External\"
+            />")
+          @header_rels_xml = header_rels.to_xml
+          insert_partial(relations_number + 1)
         end
-        @header_rels_xml = zip.read("word/_rels/header1.xml.rels")
-        @header_rels_xml = template_header_rels
-        insert_partial
       else
         # Add headers to the document
       end
@@ -56,6 +71,13 @@ module Honeydocx
         end
       end
       zip.close
+      if (!has_header_rels?)
+        Zip::File.open(save_path, Zip::File::CREATE) do |zipfile|
+          zipfile.get_output_stream('word/_rels/header1.xml.rels') do |f|
+            f.puts(@header_rels_xml)
+          end
+        end
+      end
     end
 
     def self.blank_path
@@ -73,6 +95,7 @@ module Honeydocx
     end
 
     def create_header_rels
+      # This modifies the file in place.. need to change
       zip.add('word/_rels/header1.xml.rels', WordXML.honey_header_path)
     end
 
@@ -80,16 +103,17 @@ module Honeydocx
       File.open(WordXML.honey_header_path).read
     end
 
-    def insert_partial
+    def insert_partial(rId = 1)
       # Insert hook into word/header1.xml
       partial = Nokogiri::XML(File.open(File.expand_path("../word/header1.xml.partial" , __FILE__)))
+      image_data = partial.at_xpath(".//v:imagedata")
+      image_data["r:id"] = "rId#{rId}"
       header_xml =  Nokogiri::XML(zip.read("word/header1.xml"))
       # Add header style if none exist
       if (header_xml.at_xpath(".//w:pPr").nil?)
-        binding.pry
         partial << Nokogiri::XML(File.open(File.expand_path("../word/pPr.xml", __FILE__)))
       end
-      header_xml.at_xpath(".//w:p") << partial.children[0].children
+      header_xml.at_xpath(".//w:p") << partial.children[0].children.to_xml
       @header_xml = header_xml.to_xml
     end
 
@@ -102,7 +126,7 @@ module Honeydocx
     end
 
     def has_header_rels?
-      zip.entries.any? { |entry| entry.name == "word/_rels/header1.xml.rels" }
+      zip.entries.any? { |entry| entry.name == "word/_rels/header1.xml.rels" } || !!header_rels_xml
     end
   end
 end
